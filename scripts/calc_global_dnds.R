c1ee7d4629534b81d1304669bebacb66608310c2
luisgls <luisgls@gmail.com>
2018-01-29 18:10:00 +0000

    Katz method for CI

---
+++ scripts/calc_global_dnds.R
@@ -0,0 +1,35 @@
+rm(list=ls())
+
+args <- commandArgs(trailingOnly = TRUE)
+file.names <- args[1]
+
+#Read main file
+df<-read.table(file.names, header=T, sep="\t", quote="", stringsAsFactors=F, blank.lines.skip=T, dec=".")
+
+#setwd("/Users/lzapata/Dropbox (Personal)/Dropbox_CRG/Projects/NegativeSelection/negDriver_tool/negDriver/results/")
+#df<-read.csv("testcoad.ssb192.all.finalSSB.txt",header=T,sep="\t")
+
+##Using real mutations SSB192
+m<-sum(df$nonsilent_variant)
+s<-sum(df$synonymous_variant)
+M<-sum(df$nonsynsites_SSB)
+S<-sum(df$synsites_SSB)
+
+##Calculate conf interval using Katz method
+p1<-(m/(M+1))
+p2<-(s/(S+1))
+globaldnds<-p1/p2
+N1 <- M
+N2 <- S
+
+SE = sqrt( (1-p1)/(N1*p1) + (1-p2)/(N2*p2) )
+
+finalLowCI = globaldnds * exp(-1.96*SE)
+finalHighCI = globaldnds * exp(1.96*SE)
+
+N<-m+s
+df.global<-as.data.frame(cbind(globaldnds,finalLowCI,finalHighCI,N))
+colnames(df.global)<-c("globaldnds","low_CI","high_CI","Total_Muts")
+
+write.table(df.global,file=paste(file.names,"globaldnds.txt",sep="."), quote=F,sep='\t',row.names=F)
+
