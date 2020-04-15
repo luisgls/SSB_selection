3b80ee6cb589204453d486ad3e0d8f66e313fe8e
Luis Zapata Ortiz <lortiz@login1.prv.davros.compute.estate>
2020-04-15 15:36:23 +0100

    global

--- scripts/calc_global_dnds.R
+++ scripts/calc_global_dnds.R
@@ -27,9 +27,27 @@ SE = sqrt( (1-p1)/(N1*p1) + (1-p2)/(N2*p2) )
 finalLowCI = globaldnds * exp(-1.96*SE)
 finalHighCI = globaldnds * exp(1.96*SE)
 
+#Print results
+high = finalHighCI
+low = finalLowCI
+val = globaldnds
+SE = (log(high)-log(low) )/ (2 * 1.96)
+EST = log(val)
+z = EST/SE
+PVAL1=exp(-0.717*z - 0.416*z^(2))
+PVAL2=exp(-0.717*-z - 0.416*-z^(2))
+
+if(PVAL2 > 0 & PVAL2 <= 1) {
+  PVAL = PVAL2
+} else {
+  PVAL = PVAL1
+}
+
+
+
 N<-m+s
-df.global<-as.data.frame(cbind(globaldnds,finalLowCI,finalHighCI,N))
-colnames(df.global)<-c("globaldnds","low_CI","high_CI","Total_Muts")
+df.global<-as.data.frame(cbind(globaldnds,finalLowCI,finalHighCI,N,PVAL))
+colnames(df.global)<-c("globaldnds","low_CI","high_CI","Total_Muts","p-value")
 
 write.table(df.global,file=paste(file.names,"globaldnds.txt",sep="."), quote=F,sep='\t',row.names=F)
 
